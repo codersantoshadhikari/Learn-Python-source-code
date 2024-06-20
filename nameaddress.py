7f24650b5511e64f955c685f7b99fc87d86120d3
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2024-06-20 17:36:46 +0545

    name and address print

---
+++ nameaddress.py
@@ -0,0 +1,13 @@
+	
+#  Write a Python program to add the names of your 5 friends to a list and their corresponding addresses to another list. 
+# Then, print the names and addresses in the format: "Name lives in Address."
+
+
+# Define the lists of names and addresses
+names = ['Manish Ji', 'Ram Ji', 'Hari Ji', 'Bisal Ji', 'Sita Ji']
+addresses = ['USA', 'Kathmandu', 'Pokhara', 'Bhaktapur', 'Lalitpur']
+
+
+# Loop through the lists and print the names and addresses
+for i in range(len(names)):
+    print(f"{names[i]} lives in {addresses[i]}")
