c8428f4649888b14da7d3ecc369abd766158d7a4
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2024-06-20 18:14:03 +0545

    spit

---
+++ 13string.py
@@ -0,0 +1,10 @@
+name = "Santosh Adhikari"
+lenght = len(name)
+#print(f"Lenght of {name} is {lenght}")
+print (name.capitalize())
+print (name.upper())
+print (name.lower())
+
+
+splitname = name.split()
+print(len(splitname))
---
+++ 14split.py
@@ -0,0 +1,9 @@
+full_name = "Santosh Adhikari"
+#find frist name and last name 
+spit = full_name.split()
+
+first_name = spit[0]
+last_name = spit[1]
+
+print(first_name)
+print(last_name)
