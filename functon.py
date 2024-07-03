5435c405e185e1478594a3e1a3811020ead3acf6
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2024-07-03 18:14:32 +0545

    function

---
+++ functon.py
@@ -0,0 +1,9 @@
+### Function A set of code that perfro certain task
+#function definition
+#function call
+#function with parameter
+#function with return value
+def displayName():
+    print("Santosh Adhikari")
+
+displayName()
