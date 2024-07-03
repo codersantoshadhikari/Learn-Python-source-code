cb8191c8b99463673f9bde04b187a0d9d7618fb4
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2024-07-03 18:25:49 +0545

    added

--- functon.py
+++ functon.py
@@ -13,3 +13,15 @@ def displayLocation():
     print("Pokhara - 14 Chauthe, Nepal")
 
 displayLocation()
+
+### wriete a program to print even numbers between start and end using function
+start = 100
+end = 200
+
+def displayEven(start,end):
+    for i in range(start,end):
+        if i%2 == 0:
+            print(i)
+
+displayEven(start,end)
+
