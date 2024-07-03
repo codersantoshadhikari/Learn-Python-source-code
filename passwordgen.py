0ece550cc4fbc145742d950dcdb5e65c44908716
codersantoshadhikari <santoshadhikari.name.np@gmail.com>
2024-07-03 19:16:38 +0545

    password gen custam

--- passwordgen.py
+++ passwordgen.py
@@ -1,15 +1,14 @@
     ### create a function to that gemerate randam password
     ### Option : use pramater for for no of character in password
-   ### Option : use wish give me a password of 12 character
 
 
-import random
+import random   
 import string
 
-def generate_password(length=12):
-    characters = string.ascii_lowercase + string.digits
-    password = ''.join(random.choice(characters) for _ in range(length))
-    return password
 
-password = generate_password()
+length = int(input("Enter the length of password: "))
+characters = string.ascii_letters + string.digits + '1234567890!@#$%^&*abcdefghijklmnopqrstuvwxyz()'
+
+password = ''.join(random.choice(characters) for _ in range(length))
+
 print("Generated password:", password)
